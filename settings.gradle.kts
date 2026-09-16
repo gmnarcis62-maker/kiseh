315bfa3a3811bc5db2fe729408ed7edcdf591c9d
gmnarcis62-maker <gmnarcis62@gmail.com>
2026-09-16 20:13:54 +0330

    Add GitHub Action to build APK

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,28 +1,29 @@
 pluginManagement {
   repositories {
-    google {
-      content {
-        includeGroupByRegex("com\\.android.*")
-        includeGroupByRegex("com\\.google.*")
-        includeGroupByRegex("androidx.*")
-      }
-    }
+    // 👇 آینه‌های آلی‌بابا (برای دور زدن تحریم)
+    maven { url = uri("https://maven.aliyun.com/repository/public") }
+    maven { url = uri("https://maven.aliyun.com/repository/google") }
+    maven { url = uri("https://maven.aliyun.com/repository/gradle-plugin") }
+
+    // 👇 مخازن اصلی (برای پلاگین‌های شخص ثالث مثل Roborazzi)
+    google()
     mavenCentral()
     gradlePluginPortal()
   }
 }
 
-plugins { id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0" }
-
 dependencyResolutionManagement {
   repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
   repositories {
+    maven { url = uri("https://maven.aliyun.com/repository/public") }
+    maven { url = uri("https://maven.aliyun.com/repository/google") }
     google()
     mavenCentral()
     maven { url = uri("https://jitpack.io") }
   }
 }
 
-rootProject.name = "Kiseh"
+plugins { id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0" }
 
+rootProject.name = "Kiseh"
 include(":app")
