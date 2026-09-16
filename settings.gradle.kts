5c23950d534e9f6f894f19af2e20f8ce8520df0a
gmnarcis62-maker <gmnarcis62@gmail.com>
2026-09-16 20:24:46 +0330

    Fix KSP to 2.3.6 and remove Aliyun mirrors

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,11 +1,5 @@
 pluginManagement {
   repositories {
-    // 👇 آینه‌های آلی‌بابا (برای دور زدن تحریم)
-    maven { url = uri("https://maven.aliyun.com/repository/public") }
-    maven { url = uri("https://maven.aliyun.com/repository/google") }
-    maven { url = uri("https://maven.aliyun.com/repository/gradle-plugin") }
-
-    // 👇 مخازن اصلی (برای پلاگین‌های شخص ثالث مثل Roborazzi)
     google()
     mavenCentral()
     gradlePluginPortal()
@@ -15,8 +9,6 @@ pluginManagement {
 dependencyResolutionManagement {
   repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
   repositories {
-    maven { url = uri("https://maven.aliyun.com/repository/public") }
-    maven { url = uri("https://maven.aliyun.com/repository/google") }
     google()
     mavenCentral()
     maven { url = uri("https://jitpack.io") }
